6a84c3038f46e3e01dfc69e6df81fbb2ab615038
BessonMikael <besson.mikael04@gmail.com>
2020-12-22 16:36:20 +0100

    exo 99 en cours de realisation

--- exercice1.js
+++ exercice1.js
@@ -1,8 +1,35 @@
+//I - Dans ce défi, vous allez transformer cette page Web en chronomètre. Premièrement, utilisez setTimeout pour appeler
+//la fonction countUp une fois par seconde. Puis complétez le code de la fonction countUp qui incrémente la valeur
+//de la span '#seconds' de 1 seconde à chaque fois.
+
 // Make it count up on a timer, calling this function
 let seconds = document.getElementById("seconds");
-let countUp = function() {
-    // ?
-};
+let compteur = 0;
+
+setTimeout(function (){
+    let countUp = function() {
+        if(compteur <=60){
+            seconds.innerHTML = compteur;
+            compteur++;
+        }
+    }
+},1000)
+
+
+// setInterval(function (){
+//         if(compteur <=60){
+//         let div2 = document.createElement("div");
+//         result1.append(div2);
+//         result1.style.backgroundColor = "#CCCCFF";
+//         result1.innerHTML = "Le compteur est en marche "+compteur;
+//         compteur++;
+//         }
+//     }, 1000);
+// })
+
+
+
+
 
 // How can you make it stop counting?
 let stopCountUp = function() {
